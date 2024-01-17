090a6b2caff35b6c70f053a36d1304543fbf8c5e
Jennings Zhang <jenni_zh@protonmail.com>
2024-01-17 12:39:03 -0500

    Handle stale sessions

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ See also: https://gitlab.com/openconnect/openconnect/-/issues/636
 
 ## Split-Tunneling
 
-These scripts implement both split-tunneling and split-DNS, which means they will use VPN routes only
-when necessary. For example, trying to reach http://web2.tch.harvard.edu will go over the VPN as expected.
+These scripts implement both split-tunneling and optionally split-DNS, which means they will use VPN routes
+only when necessary. For example, trying to reach http://web2.tch.harvard.edu will go over the VPN as expected.
 However, going to https://wikipedia.org or taking a Zoom call will not go over the VPN connection, which
 is better for both privacy and performance.
 
@@ -23,18 +23,18 @@ cookie which can be used by `openconnect`.
 
 ## Depencencies
 
+- sudo
 - [openconnect](https://gitlab.com/openconnect/openconnect)
 - [vpn-slice](https://github.com/dlenski/vpn-slice)
-- Python v3.9 or later
 - Node.JS v21.5.0
 - [pnpm](https://pnpm.io/)
 - Check the [Playwright system requirements](https://playwright.dev/docs/intro#system-requirements)
-- systemd-resolved (for DNS)
+- (optional, for full DNS) systemd-resolved
 
 On Arch Linux with paru or yay, you can run
 
 ```shell
-paru -S --needed openconnect vpn-slice pnpm
+paru -S --needed openconnect vpn-slice pnpm sudo
 ```
 
 ## Installation
@@ -49,33 +49,26 @@ sudo pnpm exec playwright install firefox
 
 ## Setup
 
-Duo _must_ be configured to behave one of two ways:
+Duo _must_ be configured to automatically send a push notification to your phone. It should look like:
 
-- **auto**: Duo automatically sends a push-notification to a mobile app
-- **SMS**: The first option presented by Duo is to send a passcode to a phone using SMS
+![Screenshot of Duo push](./duo_automatic_push.png)
 
 To configure Duo, log into https://vpn.childrens.harvard.edu or log into https://outlook.office365.com for the first time.
+Click "My settings & Devices" in the sidebar and select the options as shown:
 
-![Screenshot of Duo](./duo_automatic_push.png)
+![Screnshot of Duo settings](./duo_settings.png)
 
 ## Usage
 
 - `node bchvpn.js` will prompt you for your username and password, then you will receive a Duo push notification.
-- Alternatively, run `node bchvpn.js --duo sms` if using SMS for 2FA.
 - You can specify username and password as arguments: `node bchvpn.js --username chXXXXXX --password 12345678`. This is insecure.
 
 ## DNS
 
 DNS is *partially* handled. The `vpn-slice` component adds entries to `/etc/hosts`. A few important FNNDSC hostnames
-and intranet hosts are configured by default. This list can be edited in `bchvpn.js` on line 10. Or, add the
+and intranet hosts are configured by default. This list can be edited in `bchvpn.js` on line 12. Or, add the
 `-a/--append-network` option of `bchvpn.js`. For example,
 `node bchvpn.js -a centurion -a chris-next -a chris-next -a chbwiki -a rc-gitlab.chboston.org`
 
-If using `systemd-resolved`, after a connection is established, run `sudo ./dnsup.sh` to connect to the DNS servers
+Full DNS functionality with split-DNS is supported with `systemd-resolved`: after a connection is established, run `sudo ./dnsup.sh` to connect to the DNS servers
 directly, Otherwise, figure it out on your own.
-
-## Known Issues
-
-If you have a stale VPN connection, `bchvpn.js` will not work. To unblock this issue, either wait 24 hours and try again,
-or log in manually at https://vpn.childrens.harvard.edu, kill the stale session, copy the DSID cookie's value, and run
-`openconnect`.
--- bchvpn.js
+++ bchvpn.js
@@ -3,6 +3,8 @@ import { firefox } from 'playwright';
 import { expect } from '@playwright/test';
 import { read } from 'read';
 import { parseArgs } from 'node:util';
+import { execFileSync } from 'node:child_process';
+import chalk from 'chalk';
 
 /**
  * vpn-slice parameter: list of private networks and hostnames.
@@ -11,13 +13,14 @@ const DEFAULT_PRIVATE_NETWORKS = `
 10.72.0.0/16 10.36.0.0/16 10.20.0.0/16 10.26.0.0/16 134.174.12.0/24 134.174.13.0/24 10.7.34.0/24
 pangea centurion titan rc-live rc-golden chris-next galena
 web2.tch.harvard.edu chbwiki.tch.harvard.edu rc-gitlab.chboston.org hrprd.tch.harvard.edu
-`.replaceAll('\n', ' ');
+`;
 
 /**
  * Functions for handling the Duo 2-factor authentication.
  */
 const DUO_HANDLERS = {
   auto: async (page) => undefined,
+  // WARNING: I no longer use SMS so this is untested
   sms: async (page) => {
     const frame = page.frameLocator('#tp_frame').first();
 
@@ -94,43 +97,108 @@ if (values.help) {
   process.exit(1);
 }
 
-const DUO_IFRAME_POLL_OPTIONS = {
-  intervals: [1_000],
-  timeout: 30_000
-};
-
 (async () => {
   const browser = await firefox.launch();
   const page = await browser.newPage();
 
+  // Fill login page with credentials, then click "Sign In" button
   await page.goto('https://vpn.childrens.harvard.edu/dana-na/auth/url_5/welcome.cgi');
   await page.locator('#username').fill(values['username'] || await read({prompt: 'Username: '}));
   await page.locator('#password').fill(values['password'] || await read({prompt: 'Password: ', silent: true}));
   console.log();
   await page.click('#btnSubmit_6');
 
+  // Assert that Duo login pops up
   await expect
     .poll(
       async () => await page.locator('#tp_frame').getAttribute('src'),
-      DUO_IFRAME_POLL_OPTIONS
+      { timeout: 10_000 }
     )
     .toEqual(expect.stringContaining('api-9eb61ca3.duosecurity.com/frame/juniper/v2/auth'));
+  console.error(chalk.dim('Waiting for Duo 2FA'));
 
+  // handle Duo 2FA
+  // TODO select Duo handler based on text instead of cli option
+  DUO_HANDLERS[values.duo](page);
+
+  // Handle "open sessions" and DSID cookie retrieval simultaneously
+  const finishPollingForStaleSessionPage = safePollStaleSessionPage(page);
+  console.error(chalk.dim('Getting DSID cookie'));
+  const dsid = await pollforCookie(page, 'DSID');
+  await finishPollingForStaleSessionPage();
+
+  await browser.close();
+  connectToVpn(
+    'https://vpn.childrens.harvard.edu',
+    dsid,
+    values['no-vpn-slice'] ? [] : values['private-networks'].split(/\s/).filter((s) => s !== '').concat(values['append-network'])
+  );
+})();
+
+async function pollforCookie(page, cookieName) {
   await expect.poll(
     async () => {
       const cookies = await page.context().cookies();
       return cookies.map((cookie) => cookie.name);
     },
-    DUO_IFRAME_POLL_OPTIONS
-  ).toContain('DSID');
+    {
+      intervals: [1_000],
+      timeout: 60_000
+    }
+  ).toContain(cookieName);
 
   const cookies = await page.context().cookies();
-  const dsid = cookies.find((cookie) => cookie.name === 'DSID');
+  const cookie = cookies.find((cookie) => cookie.name === cookieName);
+  return cookie.value;
+}
 
-  await browser.close();
+function safePollStaleSessionPage(page) {
+  const channel = { waiting: true };
+  const promise = _staleSessionPollingLoop(page, channel);
+  return async () => {
+    channel.waiting = false;
+    await promise;
+  };
+}
 
-  const additionalNetworks = values['append-network'].length > 0 ? (' ' + values['append-network'].join(' ')) : '';
-  const s = values['no-vpn-slice'] ? '' : `-s 'vpn-slice ${values['private-networks']}${additionalNetworks}'`;
-  const cmd = `sudo openconnect --protocol=nc --cookie=${dsid.value} ${s} https://vpn.childrens.harvard.edu`;
-  console.log(cmd);
-})();
+async function _staleSessionPollingLoop(page, channel) {
+  let closedStaleSession = false;
+  while (channel.waiting && !closedStaleSession) {
+    await sleep(500);
+    if (await isOnStaleSessionPage(page)) {
+      // console.error(chalk.dim('_staleSessionPollingLoop: stale session found'));
+      await passStaleSessionPage(page);
+      console.error(chalk.dim('_staleSessionPollingLoop: stale session CLOSED'));
+      closedStaleSession = true;
+    }
+    // else {
+    //   console.error(chalk.dim('_staleSessionPollingLoop: stale session not found'));
+    // }
+  }
+  // console.error(chalk.dim('_staleSessionPollingLoop: finished'));
+}
+
+function sleep(ms) {
+  return new Promise((resolve, reject) => setTimeout(resolve, ms));
+}
+
+async function isOnStaleSessionPage(page) {
+  try {
+    await expect(page.getByText('Close Selected Sessions and Log in')).toBeAttached();
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+async function passStaleSessionPage(page) {
+  await page.locator('#postfixSID_1').check();
+  await expect(page.getByText('Close Selected Sessions and Log in')).toBeEnabled();
+  await page.getByText('Close Selected Sessions and Log in').click();
+}
+
+function connectToVpn(vpnUrl, dsid, vpnSliceArguments) {
+  const s = vpnSliceArguments.length === 0 ? [] : ['-s', 'vpn-slice ' + vpnSliceArguments.join(' ')];
+  const cmd = ['openconnect', '--protocol=nc', '--cookie', dsid, ...s, vpnUrl];
+  execFileSync('sudo', cmd, { stdio: 'inherit' });
+}
